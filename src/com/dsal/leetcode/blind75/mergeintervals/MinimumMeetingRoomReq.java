244d066e108e904fb3651b5ff3adaa323138986d
ankitasinha0530-root <45222337+ankitasinha0530-root@users.noreply.github.com>
2026-05-20 06:24:10 +0530

    Adding new code

--- src/com/dsal/leetcode/blind75/mergeintervals/MinimumMeetingRoomReq.java
+++ src/com/dsal/leetcode/blind75/mergeintervals/MinimumMeetingRoomReq.java
@@ -2,6 +2,9 @@ package com.dsal.leetcode.blind75.mergeintervals;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class MinimumMeetingRoomReq {
     public static void main(String[] args) {
@@ -12,18 +15,23 @@ public class MinimumMeetingRoomReq {
 
 
         int[][] intervals = {{6, 7}, {2, 5}, {8, 12}}; // output = 1
-        System.out.println(noOfMeetingRoomRequired(intervals)); // min no.of meeting rooms required
+        System.out.println(noOfMeetingRoomRequiredTwoPtr(intervals)); // min no.of meeting rooms required
+        System.out.println(noOfMeetingRoomRequiredMinHeap(intervals));
 
+        System.out.println();
         int[][] intervals1 = {{1, 4}, {2, 5}, {7, 9}}; // output = 2
-        System.out.println(noOfMeetingRoomRequired(intervals1));
+        System.out.println(noOfMeetingRoomRequiredTwoPtr(intervals1));
+        System.out.println(noOfMeetingRoomRequiredMinHeap(intervals1));
 
+        System.out.println();
         int[][] intervals2 = {{0, 30}, {5, 10}, {15, 20}}; // output = 2
-        System.out.println(noOfMeetingRoomRequired(intervals2));
+        System.out.println(noOfMeetingRoomRequiredTwoPtr(intervals2));
+        System.out.println(noOfMeetingRoomRequiredMinHeap(intervals2));
     }
 
     // We have to focus on end time of the meeting
 
-    private static int noOfMeetingRoomRequired(int[][] intervals) {
+    private static int noOfMeetingRoomRequiredTwoPtr(int[][] intervals) {
 
         int n = intervals.length;
         int[] startIntervals = new int[n];
@@ -39,12 +47,31 @@ public class MinimumMeetingRoomReq {
         int endPointer = 0;
 
         for (int i = 0; i < n; i++) {
-            if(startIntervals[i] < endIntervals[endPointer]){ // check if meeting started before the 1st meeting ended
+            if (startIntervals[i] < endIntervals[endPointer]) { // check if meeting started before the 1st meeting ended
                 room++;       // if yes then the meeting would require new room
-            }else {
+            } else {
                 endPointer++; // else next meeting can continue in same room.
             }
         }
         return room;
+    }
+
+    private static int noOfMeetingRoomRequiredMinHeap(int[][] intervals) {
+
+        int minMeetingRooms = 0;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(intervals.length, Comparator.comparing(i -> i[1]));
+
+        for (int[] interval : intervals) {
+            while (!minHeap.isEmpty() && interval[0] >= minHeap.peek()[1]) {  // remove all meetings that have ended before this iteration meeting start from minHeap
+                minHeap.poll(); // if not overlapping then we dont need more rooms, we can have multiple meetings in 1 single room
+            }
+            minHeap.offer(interval); // add the current meeting into the minHeap
+            minMeetingRooms = Math.max(minMeetingRooms, minHeap.size()); // at every interval calculate the min meeting rooms
+        }
+
+
+        return minMeetingRooms;
     }
 }
